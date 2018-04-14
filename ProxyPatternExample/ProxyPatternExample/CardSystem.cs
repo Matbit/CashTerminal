1b639c41266195b7058a1e6b01df5ee12632b1e6
Matbit <codesharing@gmx.de>
2018-04-14 19:47:03 +0200

    CashTerminal -0001

---
+++ ProxyPatternExample/ProxyPatternExample/CardSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPatternExample
+{
+    class CardSystem
+    {
+        private static Card cardIn = Card.NO_Card;
+
+        //no instances
+        private CardSystem()
+        {
+
+        }
+
+
+        public enum Card
+        {
+            NO_Card, OK
+        }
+
+        public static Card getCardStatus()
+        {
+            return cardIn;
+        }
+
+        public static void setCardStatus(bool isThere)
+        {
+            if (isThere)
+                cardIn = Card.OK;
+            else cardIn = Card.NO_Card;
+        }
+
+    }
+}
---
+++ ProxyPatternExample/ProxyPatternExample/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProxyPatternExample
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPutCardIn_Click(object sender, EventArgs e)
+        {
+            if(CardSystem.getCardStatus() == CardSystem.Card.OK)
+            {
+                tbxProgram.AppendText("Can not read Card. Card is still in progress." + Environment.NewLine);
+                return;
+            }
+            
+
+        }
+    }
+}
